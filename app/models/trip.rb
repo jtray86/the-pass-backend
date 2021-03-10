8fd57bd95bc166d9909f28e3b603a0781380b768
Melissa Faulner <melissa.faulner@gmail.com>
2021-03-10 14:09:49 -0500

    fixed Trip seeding issue by adding optional: true to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
-  belongs_to :user_as_owner, class_name: 'User', foreign_key:'owner_id'
-  belongs_to :user_as_traveler, class_name: 'User', foreign_key: 'traveler_id'
+  belongs_to :owner, class_name: 'User'
+  belongs_to :traveler, class_name: 'User', optional: true
   
   validates_presence_of :name, :city, :country, :start_date, :end_date, :description, :owner_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :owner_trips, class_name: 'Trip'
-    has_many :traveler_trips, class_name: 'Trip'
+    has_many :owner_trips, class_name: 'Trip', foreign_key:'owner_id'
+    has_many :traveler_trips, class_name: 'Trip', foreign_key: 'traveler_id'
 
     validates :username, uniqueness: { case_sensitive: false }
     validates :email, uniqueness: { case_sensitive: false }
